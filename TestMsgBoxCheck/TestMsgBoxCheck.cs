1cd53b490a7725556e26e6e09df01d5b9d8732a0
gyco50 <gyco50@outlook.com>
2017-11-21 15:30:32 +0100

    syntax clean

--- TestMsgBoxCheck/TestMsgBoxCheck.cs
+++ TestMsgBoxCheck/TestMsgBoxCheck.cs
@@ -9,13 +9,13 @@ namespace TestMsgBoxCheck
 	/// </summary>
 	public class TestMsgBoxCheckForm : System.Windows.Forms.Form
 	{
-		private System.Windows.Forms.Button btnTest;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.Button _btnTest;
+		private System.Windows.Forms.Label _label1;
+		private System.Windows.Forms.Button _btnClear;
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		private System.ComponentModel.Container components = null;
+		private readonly System.ComponentModel.Container _components = null;
 
 	    private TestMsgBoxCheckForm()
 		{
@@ -36,7 +36,7 @@ namespace TestMsgBoxCheck
 		{
 			if( disposing )
 			{
-			    components?.Dispose();
+			    _components?.Dispose();
 			}
 			base.Dispose( disposing );
 		}
@@ -48,46 +48,46 @@ namespace TestMsgBoxCheck
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.btnTest = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.btnClear = new System.Windows.Forms.Button();
+			this._btnTest = new System.Windows.Forms.Button();
+			this._label1 = new System.Windows.Forms.Label();
+			this._btnClear = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// btnTest
 			// 
-			this.btnTest.Location = new System.Drawing.Point(8, 96);
-			this.btnTest.Name = "btnTest";
-			this.btnTest.Size = new System.Drawing.Size(128, 23);
-			this.btnTest.TabIndex = 0;
-			this.btnTest.Text = "Test MsgBoxCheck";
-			this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+			this._btnTest.Location = new System.Drawing.Point(8, 96);
+			this._btnTest.Name = "_btnTest";
+			this._btnTest.Size = new System.Drawing.Size(128, 23);
+			this._btnTest.TabIndex = 0;
+			this._btnTest.Text = "Test MsgBoxCheck";
+			this._btnTest.Click += new System.EventHandler(this.btnTest_Click);
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(8, 8);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(264, 80);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Press the button below to test the MsgBoxCheck class.  If you check the \"Don\'t sh" +
+			this._label1.Location = new System.Drawing.Point(8, 8);
+			this._label1.Name = "_label1";
+			this._label1.Size = new System.Drawing.Size(264, 80);
+			this._label1.TabIndex = 1;
+			this._label1.Text = "Press the button below to test the MsgBoxCheck class.  If you check the \"Don\'t sh" +
 				"ow me this again\" box pressing the button below will have no affect, unless you " +
 				"use the other button to clear the registry variable.";
 			// 
 			// btnClear
 			// 
-			this.btnClear.Location = new System.Drawing.Point(144, 96);
-			this.btnClear.Name = "btnClear";
-			this.btnClear.Size = new System.Drawing.Size(120, 23);
-			this.btnClear.TabIndex = 2;
-			this.btnClear.Text = "Clear Registry";
-			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+			this._btnClear.Location = new System.Drawing.Point(144, 96);
+			this._btnClear.Name = "_btnClear";
+			this._btnClear.Size = new System.Drawing.Size(120, 23);
+			this._btnClear.TabIndex = 2;
+			this._btnClear.Text = "Clear Registry";
+			this._btnClear.Click += new System.EventHandler(this.btnClear_Click);
 			// 
 			// TestMsgBoxCheckForm
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
 			this.ClientSize = new System.Drawing.Size(280, 126);
-			this.Controls.Add(this.btnClear);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.btnTest);
+			this.Controls.Add(this._btnClear);
+			this.Controls.Add(this._label1);
+			this.Controls.Add(this._btnTest);
 			this.Name = "TestMsgBoxCheckForm";
 			this.Text = "TestMsgBoxCheck";
 			this.ResumeLayout(false);
@@ -113,8 +113,8 @@ namespace TestMsgBoxCheck
 
 		private void btnClear_Click(object sender, System.EventArgs e)
 		{
-			RegistryKey regKey = Registry.CurrentUser.CreateSubKey(@"Software\PricklySoft\TestMsgBoxCheck");
-			regKey.SetValue("DontShowAgain",false);
+		    RegistryKey regKey = Registry.CurrentUser.CreateSubKey(@"Software\PricklySoft\TestMsgBoxCheck");
+		    regKey?.SetValue("DontShowAgain", false);
 		}
 	}
 }
